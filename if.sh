cd7e29dfb9575268b0efd8e5256ddecba56c73de
ramakrishna5822 <ramakrishna5822@gmail.com>
2019-04-10 07:22:06 +0530

    adding if.sh

--- if.sh
+++ if.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 int1=$1
 int2=$2
-if [ $int1 -eq $int2 ];then
-echo "values are equal"
+if [ $int1 -gt $int2 ];then
+echo "values are grater"
 else
-echo "values are not equal"
+echo "values are not grater"
 fi
